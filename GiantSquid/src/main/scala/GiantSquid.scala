462172c14f12ca5539ab3cd499a001a0f68c4a0d
Roman Mendaliev <reacheight8@gmail.com>
2021-12-05 00:28:03 +0300

    Add solution for day 4

---
+++ GiantSquid/src/main/scala/GiantSquid.scala
@@ -0,0 +1,24 @@
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Using
+
+object GiantSquid {
+  def main(args: Array[String]): Unit = {
+    val input = Using.Manager { use => use(Source.fromFile("src/main/input.txt")).mkString.split("\r\n\r\n") }.get
+    val numbers = input.head.split(',').map(_.toInt)
+    val boards = input.drop(1).map(board => board.split('\n').map(row => row.trim.split(" +") .map(_.toIntOption)))
+
+    println(calculateScore(firstWin = true)(boards, numbers))
+    println(calculateScore(firstWin = false)(boards, numbers))
+  }
+
+  @tailrec
+  def calculateScore(firstWin:  Boolean)(boards: Array[Array[Array[Option[Int]]]], numbers: Array[Int]): Int = {
+    val marked = boards.map(board => board.map(row => row.map(number => if (number.contains(numbers.head)) None else number)))
+    val (won, others) = marked.partition(board => (board ++ board.transpose).exists(_.flatten.isEmpty))
+    if (!won.isEmpty && firstWin || others.isEmpty && !firstWin)
+      won.head.flatten.flatten.sum * numbers.head
+    else
+      calculateScore(firstWin)(others, numbers.tail)
+  }
+}
